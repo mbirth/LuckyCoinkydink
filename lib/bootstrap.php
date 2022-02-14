5bd24c2d12c71cbdb6e639c1d896ee4ba3f0d07a
Markus Birth <mbirth@gmail.com>
2022-02-14 21:44:20 +0100

    Output contents of $serendipity on Exception.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -6,6 +6,11 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // TODO: REMOVE WHEN DONE DEVELOPING
+$whoops_handler = new \Whoops\Handler\PrettyPageHandler();
+$whoops_handler->addDataTableCallback('$serendipity', function () {
+    global $serendipity;
+    return $serendipity;
+});
 $whoops = new \Whoops\Run();
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+$whoops->pushHandler($whoops_handler);
 $whoops->register();
