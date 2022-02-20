d18224e34d364f4b868a46dc76f3b97b4ad021a5
Markus Birth <mbirth@gmail.com>
2022-02-20 17:15:52 +0100

    Improve bootstrap.php.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -3,7 +3,16 @@
 // 幸運な偶然 - Lucky Coinkydink
 // See LICENSE file for license information.
 
-require_once __DIR__ . '/../vendor/autoload.php';
+namespace LuckyCoin;
+
+// Ensure vendor libraries exist
+$autoload = __DIR__ . '/../vendor/autoload.php';
+if (!is_file($autoload)) {
+    die('Please run: <i>./composer.phar install</i>');
+}
+
+// Register the auto-loader.
+$loader = require $autoload;
 
 // TODO: REMOVE WHEN DONE DEVELOPING
 $whoops_handler = new \Whoops\Handler\PrettyPageHandler();
@@ -14,3 +23,11 @@ $whoops_handler->addDataTableCallback('$serendipity', function () {
 $whoops = new \Whoops\Run();
 $whoops->pushHandler($whoops_handler);
 $whoops->register();
+
+// Set timezone to default, falls back to system if php.ini not set
+date_default_timezone_set(@date_default_timezone_get());
+
+// Set internal encoding.
+@ini_set('default_charset', 'UTF-8');
+mb_language('uni');
+mb_internal_encoding('UTF-8');
